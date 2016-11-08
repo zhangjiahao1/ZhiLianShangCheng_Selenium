5239ea02500dca7161d6dd7e62b99f9d6f58ef47
zhangjiahao <a1257524459@163.com>
2016-11-08 22:25:04 +0800

    魔鏡

--- src/cn/Wolf/appModules/niaho.java
+++ src/cn/Wolf/appModules/niaho.java
@@ -4,7 +4,7 @@ public class niaho {
 	public static void hhe(){
 		System.out.println("wuwuda");
 		System.out.println("这是什么东西！！！！！");
-		//hahhahahha
+		//hahhahahha 的雙方各單方事故
 		System.out.println("wtf");
 	}
 }
